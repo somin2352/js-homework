1e9e4ad4340d654c394d756e2da120c5c62ae721
somin2352 <somin030502@gmail.com>
2024-11-04 10:10:20 +0900

    과제1 코드 리뷰

--- mission01/mission01-1.js
+++ mission01/mission01-1.js
@@ -1,8 +1,18 @@
+// 함수는 매개변수를 유연하게 받을수록 재사용성이 증가
+// Object.prototype.toString.call().slice(8,-1).toLowerCase() -> 값의 타입을 정확하게 확인할 수 있는 방법
 function getValueAtObject(obj, key) {
+  if (Object.prototype.toString.call(object).slice(8, -1).toLowerCase() === "object") {
+    throw new TypeError("첫 번째 인수는 객체 타입이어야 합니다.");
+  }
+
+  if (!(typeof key === "string")) {
+    throw new TypeError("두 번째 인수는 문자 타입이어야 합니다.");
+  }
+
   if (Object.keys(obj).includes(key)) {
     return obj[key];
   } else {
-    console.error("Error!");
+    throw new SyntaxError("Error!");
   }
 }
 
@@ -22,4 +32,23 @@ console.log(getValueAtObject(person, "country")); // Error !
 
 // 2. key in obj
 
-// 3. Object.hasOwnProperty(key)
+// 3. Object.hasOwnProperty(key) -> 안전하게 받을 수 있는 방법
+
+function _getValueAtObject(object, key) {
+  const entries = Object.entries(object);
+
+  // for(let [k,v] of entries) if(k === key) return v
+
+  // entries.forEach([k,v]) => k === key ? value = v : ''
+
+  for (let keyValue of entries) {
+    const _key = keyValue[0];
+    const value = keyValue[1];
+
+    if (_key === key) {
+      return value;
+    }
+  }
+}
+
+console.log(_getValueAtObject(obj, "name"));
--- mission01/mission01-2.js
+++ mission01/mission01-2.js
@@ -1,4 +1,9 @@
 function getNumberAtArray(arr, index) {
+  // isArray(): 배열인지를 확인할 수 있는 함수
+  if (!Array.isArray(array)) {
+    throw new Error("첫 번째 인수는 배열 타입이어야 합니다.");
+  }
+
   if (0 <= index && index < arr.length) {
     // 0 <= index < arr.length 불가
     return arr[index];
